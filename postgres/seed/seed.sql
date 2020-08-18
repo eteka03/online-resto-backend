39b81ccf3d36884d701c18d8b28e45304d426e3c
eteka03 <eteka03.akpaki@gmail.com>
2020-08-18 13:24:53 -0400

    fix docker file and add postgres service

---
+++ postgres/seed/seed.sql
@@ -0,0 +1,6 @@
+BEGIN TRANSACTION;
+
+INSERT into users (name,email, entries,joined) values ('abel','abel@gmail.com',7,'2020-03-03');
+
+
+COMMIT
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 //initialize server
+require('dotenv').config()
 const express =  require('express')
-
 const app = express()
 const cors = require('cors')
+const db = require('knex')({
+    client:'pg',
+    connection: {
+        host:process.env.POSTGRES_HOST,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB
+      }
+})
+
+
 
 
 //middleware
@@ -11,7 +22,25 @@ app.use(cors())
 
 
 app.get('/',(req,res)=>{
-   return res.send('hello world')
+
+    
+    
+    
+
+    db.from('users')
+    .select('*')
+    .where({id:1})
+    .then(data => {
+        console.log('data',data)
+        return res.send('succes')
+    })
+    .catch(err => {
+        console.log('user',process.env.POSTGRES_USER)
+        console.log(err)
+        return res.status(500).json({message:"booo"})
+    })
+
+    
 })
 
 app.listen(5001,()=>{
